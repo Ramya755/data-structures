3638a4132e62e2abfe2d60ca5f4ed910bdb1c424
Ramya755 <dosapadularamya@gmail.com>
2025-12-07 13:32:09 +0530

    2nd

---
+++ matrices/transpose_of _matrix.py
@@ -0,0 +1,11 @@
+A = [[1, 2, 3],
+     [4, 5, 6]]
+
+transpose = []
+
+for i in range(len(A[0])):  
+    row = []
+    for j in range(len(A)):  
+        row.append(A[j][i])
+    transpose.append(row)
+print(transpose)
