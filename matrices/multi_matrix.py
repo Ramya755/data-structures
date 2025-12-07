ea0253a39e592dc7311d620d1b532572d4751bef
Ramya755 <dosapadularamya@gmail.com>
2025-12-07 13:34:50 +0530

    3rd

---
+++ matrices/multi_matrix.py
@@ -0,0 +1,15 @@
+A = [[1,2,3],
+     [4,5,6]]
+
+B = [[7,8],
+     [9,10],
+     [1,2]]
+
+result = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+
+for i in range(len(A)):           
+    for j in range(len(B[0])):     
+        for k in range(len(B)):  
+            result[i][j] += A[i][k] * B[k][j]
+
+print(result)
