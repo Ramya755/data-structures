aaa638b9d01b5c04e0296b7a29650127c7a3d874
Ramya755 <dosapadularamya@gmail.com>
2025-12-08 21:51:43 +0530

    73_brute_force

---
+++ matrices/73_set_zero_brute_force.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    for k in range(len(matrix[i])):
+                        if matrix[i][k]!=0:
+                            matrix[i][k]=-1
+                    for l in range(len(matrix)):
+                        if matrix[l][j]!=0:
+                            matrix[l][j]=-1
+        for a in range(len(matrix)):
+            for b in range(len(matrix[0])):
+                if matrix[a][b]==-1:
+                    matrix[a][b]=0
+        return matrix
+sol=Solution()
+print(sol.setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]]))
