cd4df85daf1d7e23f75d10f9a38ae08e2f25a5de
Ramya755 <dosapadularamya@gmail.com>
2025-12-23 22:11:32 +0530

    insertion_sort

---
+++ sorting/insertion_sort_gfg.py
@@ -0,0 +1,9 @@
+def insertion_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        j=i
+        while j>0 and arr[j-1]>arr[j]:
+            arr[j-1],arr[j]=arr[j],arr[j-1]
+            j-=1
+    return arr
+print(insertion_sort([5,8,1,4,15,6]))
