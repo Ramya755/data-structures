4b65989a815d6962dc85664e965eb1735fb4296b
Ramya755 <dosapadularamya@gmail.com>
2025-12-25 12:21:00 +0530

    comment

--- sorting/insertion_sort_gfg.py
+++ sorting/insertion_sort_gfg.py
@@ -5,5 +5,6 @@ def insertion_sort(arr):
         while j>0 and arr[j-1]>arr[j]:
             arr[j-1],arr[j]=arr[j],arr[j-1]
             j-=1
+            #it will check before elements like it starts from 8 actually when it went to 1 it will check upto 5 and put 1 at first
     return arr
 print(insertion_sort([5,8,1,4,15,6]))
