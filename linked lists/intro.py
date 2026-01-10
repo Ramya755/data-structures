0585b0112e51c37e57d93e4bf7d8a947b5eb0de9
Ramya755 <dosapadularamya@gmail.com>
2026-01-10 20:08:50 +0530

    2

--- linked lists/intro.py
+++ linked lists/intro.py
@@ -13,11 +13,21 @@ class LinkedList:
             print(temp.data,end="-->")
             temp=temp.next
         print("None")
+    def insertAtEnd(self,head,data):
+        newNode=Node(data)
+        temp=head
+        if head is None:
+            return newNode
+        while temp.next:
+            temp=temp.next
+        temp.next=newNode
+        return head
 sol=LinkedList()
 head=None
 head=sol.insertAtBeginning(head,10)
 head=sol.insertAtBeginning(head,20)
 head=sol.insertAtBeginning(head,30)
+head=sol.insertAtEnd(head,70)
 sol.printlist(head)
 
         
